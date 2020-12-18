849c6be5b2d74a48dd59ec1fbac6b4cbd3da8eeb
Alan Tseng <atseng202@gmail.com>
2020-12-18 12:10:42 -0800

    Add modal feature for adding new message

--- app.py
+++ app.py
@@ -36,10 +36,12 @@ def add_user_to_g():
     if CURR_USER_KEY in session:
         g.user = User.query.get(session[CURR_USER_KEY])
         g.delete_or_logout_form = DeleteOrLogoutForm()
+        g.message_form = MessageForm()
 
     else:
         g.user = None
         g.delete_or_logout_form = None
+        g.message_form = MessageForm()
 
 
 def do_login(user):
